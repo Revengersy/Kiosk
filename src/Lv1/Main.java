1b9677fc1ad1211c72f6fafa0e6df9fd7753eca3
JunChoi <chldnjswns1@daum.net>
2024-11-21 12:22:00 +0900

    :sparkles:feat: complete Lv1

--- src/Lv1/Main.java
+++ src/Lv1/Main.java
@@ -1,5 +1,6 @@
 package Lv1;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -15,18 +16,39 @@ public class Main {
         double[] menuPrices = {0.0, 6.9, 8.9, 6.9, 5.4};
         String[] menuTexts = {"종료", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", "비프패티를 기반으로 야채가 들어간 기본버거"};
 
-        System.out.println("[SHAKESHACK MENU]");
-        for (int i = 0; i < menuIndexes.length; i++) {
-            System.out.printf("%d. %s | W %f | %s %n", menuIndexes[i], menuNames[i], menuPrices[i], menuTexts[i]);
-        }
+        while (true) {
+            System.out.println("[SHAKESHACK MENU]");
+            for (int i = 0; i < menuIndexes.length; i++) {
+                System.out.printf("%d. %s | W %f | %n", menuIndexes[i], menuNames[i], menuPrices[i]);
+            }
 
-        String tmp = getVaildInputWithRegex("메뉴 번호를 입력해 주세요(0-4)", "[0-9]");
-        int userInput = Integer.parseInt(tmp);
-        System.out.println("입력된 숫자: " + userInput);
+            String tmp = getVaildInputWithRegex("메뉴 번호를 입력해 주세요(0-4)", "[0-4]");
+            int userInput = Integer.parseInt(tmp);
+            System.out.printf("입력된 메뉴: %d. %s %n", userInput, menuNames[userInput]);
 
+            switch (userInput) {
+                case 0:
+                    System.out.println("종료합니다");
+                    break;
+                case 1:
+                    System.out.println(menuTexts[userInput]);
+                    break;
+                case 2:
+                    System.out.println(menuTexts[userInput]);
+                    break;
+                case 3:
+                    System.out.println(menuTexts[userInput]);
+                    break;
+                case 4:
+                    System.out.println(menuTexts[userInput]);
+                    break;
+            }
+            System.out.println();
 
-        if(userInput == 0) {
-            System.out.println("종료합니다");
+            if (userInput == 0) {
+                System.out.println("종료합니다");
+                break;
+            }
         }
 
 
